2bb8302a1e7396a75c74d8215774a41c99416c3e
Robert Edmonds <edmonds@users.noreply.github.com>
2023-11-23 16:39:24 -0500

    Add "cfg_" prefix on global variables used for configuration values

--- src/NixieTap.cpp
+++ src/NixieTap.cpp
@@ -39,20 +39,20 @@ WiFiManager wifiManager;
 time_t t;
 String serialCommand = "";
 
-uint8 timeRefreshFlag;
+uint8_t timeRefreshFlag;
 
 const char *NixieTap = "NixieTap";
 
-char _time[6] = "00:00";
-char date[11] = "1970-01-01";
-char SSID[50] = "NixieTap";
-char password[50] = "nixietap";
-char target_SSID[50] = "\0";
-char target_pw[50] = "\0";
-uint8 enable_time = 1;
-uint8 enable_date = 1;
-uint8 manual_time_flag = 1;
-uint8 enable_24h = 1;
+char cfg_time[6] = "00:00";
+char cfg_date[11] = "1970-01-01";
+char cfg_SSID[50] = "NixieTap";
+char cfg_password[50] = "nixietap";
+char cfg_target_SSID[50] = "\0";
+char cfg_target_pw[50] = "\0";
+uint8_t cfg_enable_time = 1;
+uint8_t cfg_enable_date = 1;
+uint8_t cfg_manual_time_flag = 1;
+uint8_t cfg_enable_24h = 1;
 
 std::map<String, int> mem_map;
 
@@ -115,10 +115,10 @@ void setup()
 	enableSecDot();
 
 	// Connect to WiFi.
-	if (target_SSID[0] != '\0' && target_pw[0] != '\0') {
+	if (cfg_target_SSID[0] != '\0' && cfg_target_pw[0] != '\0') {
 		Serial.print("Connecting to Wi-Fi access point: ");
-		Serial.println(target_SSID);
-		wifiManager.connectWifi(target_SSID, target_pw);
+		Serial.println(cfg_target_SSID);
+		wifiManager.connectWifi(cfg_target_SSID, cfg_target_pw);
 	}
 
 	// Progress bar: 100%.
@@ -135,7 +135,7 @@ void loop()
 	t = now(); // update date and time variable
 
 	// If time is configured to be set semi-auto or auto and NixieTap is just started, the NTP client is started.
-	if (manual_time_flag == 0 && wifiFirstConnected && WiFi.status() == WL_CONNECTED) {
+	if (cfg_manual_time_flag == 0 && wifiFirstConnected && WiFi.status() == WL_CONNECTED) {
 		NTP.onNTPSyncEvent([](NTPSyncEvent_t event) {
 			ntpEvent = event;
 			syncEventTriggered = true;
@@ -155,15 +155,15 @@ void loop()
 		state = 0;
 
 	// Slot 0 - time
-	if (state == 0 && enable_time) {
-		nixieTap.writeTime(t, dot_state, enable_24h);
-	} else if (!enable_time && state == 0)
+	if (state == 0 && cfg_enable_time) {
+		nixieTap.writeTime(t, dot_state, cfg_enable_24h);
+	} else if (!cfg_enable_time && state == 0)
 		state++;
 
 	// Slot 1 - date
-	if (state == 1 && enable_date) {
+	if (state == 1 && cfg_enable_date) {
 		nixieTap.writeDate(t, 1);
-	} else if (!enable_date && state == 1)
+	} else if (!cfg_enable_date && state == 1)
 		state++;
 
 	// Here you can add new functions for displaying numbers on NixieTap, just follow the basic writing principle from above.
@@ -177,7 +177,7 @@ void startPortalManually()
 	movingDot.attach(0.2, scrollDots);
 	wifiManager.setConfigPortalTimeout(1800);
 	// This will run a new config portal if the SSID and PW are valid.
-	if (!wifiManager.startConfigPortal(SSID, password)) {
+	if (!wifiManager.startConfigPortal(cfg_SSID, cfg_password)) {
 		Serial.println("Failed to connect and hit timeout!");
 		// If the NixieTap is not connected to WiFi, it will collect the entered parameters and configure the RTC according to them.
 	}
@@ -218,44 +218,44 @@ void readParameters()
 	Serial.println("Reading saved parameters from EEPROM.");
 
 	int EEaddress = mem_map["SSID"];
-	EEPROM.get(EEaddress, SSID);
+	EEPROM.get(EEaddress, cfg_SSID);
 	Serial.print("[EEPROM Read] ");
-	Serial.println("SSID: " + (String)SSID);
+	Serial.println("SSID: " + (String)cfg_SSID);
 
 	EEaddress = mem_map["password"];
-	EEPROM.get(EEaddress, password);
+	EEPROM.get(EEaddress, cfg_password);
 	Serial.print("[EEPROM Read] ");
-	Serial.println("password: " + (String)password);
+	Serial.println("password: " + (String)cfg_password);
 
 	EEaddress = mem_map["target_ssid"];
-	EEPROM.get(EEaddress, target_SSID);
+	EEPROM.get(EEaddress, cfg_target_SSID);
 	Serial.print("[EEPROM Read] ");
-	Serial.println("target_ssid: " + (String)target_SSID);
+	Serial.println("target_ssid: " + (String)cfg_target_SSID);
 
 	EEaddress = mem_map["target_pw"];
-	EEPROM.get(EEaddress, target_pw);
+	EEPROM.get(EEaddress, cfg_target_pw);
 	Serial.print("[EEPROM Read] ");
-	Serial.println("target_pw: " + (String)target_pw);
+	Serial.println("target_pw: " + (String)cfg_target_pw);
 
 	EEaddress = mem_map["manual_time_flag"];
-	EEPROM.get(EEaddress, manual_time_flag);
+	EEPROM.get(EEaddress, cfg_manual_time_flag);
 	Serial.print("[EEPROM Read] ");
-	Serial.println("manual_time_flag: " + (String)manual_time_flag);
+	Serial.println("manual_time_flag: " + (String)cfg_manual_time_flag);
 
 	EEaddress = mem_map["enable_date"];
-	EEPROM.get(EEaddress, enable_date);
+	EEPROM.get(EEaddress, cfg_enable_date);
 	Serial.print("[EEPROM Read] ");
-	Serial.println("enable_date: " + (String)enable_date);
+	Serial.println("enable_date: " + (String)cfg_enable_date);
 
 	EEaddress = mem_map["enable_time"];
-	EEPROM.get(EEaddress, enable_time);
+	EEPROM.get(EEaddress, cfg_enable_time);
 	Serial.print("[EEPROM Read] ");
-	Serial.println("enable_time: " + (String)enable_time);
+	Serial.println("enable_time: " + (String)cfg_enable_time);
 
 	EEaddress = mem_map["enable_24h"];
-	EEPROM.get(EEaddress, enable_24h);
+	EEPROM.get(EEaddress, cfg_enable_24h);
 	Serial.print("[EEPROM Read] ");
-	Serial.println("enable_24h: " + (String)enable_24h);
+	Serial.println("enable_24h: " + (String)cfg_enable_24h);
 }
 
 void updateParameters()
@@ -266,72 +266,72 @@ void updateParameters()
 	Serial.println("Comparing entered keys with the saved ones.");
 	if (wifiManager.nixie_params.count("SSID") == 1) {
 		const char *nixie_ssid = wifiManager.nixie_params["SSID"].c_str();
-		if (nixie_ssid[0] != '\0' and SSID != nixie_ssid) {
+		if (nixie_ssid[0] != '\0' and cfg_SSID != nixie_ssid) {
 			EEaddress = mem_map["SSID"];
-			strcpy(SSID, nixie_ssid);
-			EEPROM.put(EEaddress, SSID);
+			strcpy(cfg_SSID, nixie_ssid);
+			EEPROM.put(EEaddress, cfg_SSID);
 			const char *nixie_pw = wifiManager.nixie_params["hotspot_password"].c_str();
-			if (nixie_pw[0] != '\0' and password != nixie_pw) {
+			if (nixie_pw[0] != '\0' and cfg_password != nixie_pw) {
 				EEaddress = mem_map["password"];
-				strcpy(password, nixie_pw);
-				EEPROM.put(EEaddress, password);
+				strcpy(cfg_password, nixie_pw);
+				EEPROM.put(EEaddress, cfg_password);
 			}
 		}
 	}
 	if (wifiManager.nixie_params.count("target_ssid") == 1) {
 		const char *new_target_ssid = wifiManager.nixie_params["target_ssid"].c_str();
-		if (new_target_ssid[0] != '\0' and target_SSID != new_target_ssid) {
+		if (new_target_ssid[0] != '\0' and cfg_target_SSID != new_target_ssid) {
 			EEaddress = mem_map["target_ssid"];
-			strcpy(target_SSID, new_target_ssid);
-			EEPROM.put(EEaddress, target_SSID);
+			strcpy(cfg_target_SSID, new_target_ssid);
+			EEPROM.put(EEaddress, cfg_target_SSID);
 			const char *new_target_pw = wifiManager.nixie_params["target_password"].c_str();
-			if (new_target_pw[0] != '\0' and new_target_pw != target_pw) {
+			if (new_target_pw[0] != '\0' and new_target_pw != cfg_target_pw) {
 				EEaddress = mem_map["target_pw"];
-				strcpy(target_pw, new_target_pw);
-				EEPROM.put(EEaddress, target_pw);
-				wifiManager.connectWifi(target_SSID, target_pw);
+				strcpy(cfg_target_pw, new_target_pw);
+				EEPROM.put(EEaddress, cfg_target_pw);
+				wifiManager.connectWifi(cfg_target_SSID, cfg_target_pw);
 			}
 		}
 	}
 	uint8_t new_enable_date = (uint8_t)wifiManager.nixie_params.count("enableDate");
-	if (new_enable_date != enable_date) {
+	if (new_enable_date != cfg_enable_date) {
 		EEaddress = mem_map["enable_date"];
-		enable_date = new_enable_date;
-		EEPROM.put(EEaddress, enable_date);
+		cfg_enable_date = new_enable_date;
+		EEPROM.put(EEaddress, cfg_enable_date);
 	}
 	uint8_t new_enable_time = (uint8_t)wifiManager.nixie_params.count("enableTime");
-	if (new_enable_time != enable_time) {
+	if (new_enable_time != cfg_enable_time) {
 		EEaddress = mem_map["enable_time"];
-		enable_time = new_enable_time;
+		cfg_enable_time = new_enable_time;
 		EEPROM.put(EEaddress, new_enable_time);
 	}
 	uint8_t new_enable_24h = (uint8_t)wifiManager.nixie_params.count("enable24h");
-	if (enable_24h != new_enable_24h) {
+	if (cfg_enable_24h != new_enable_24h) {
 		EEaddress = mem_map["enable_24h"];
-		enable_24h = new_enable_24h;
-		EEPROM.put(EEaddress, enable_24h);
+		cfg_enable_24h = new_enable_24h;
+		EEPROM.put(EEaddress, cfg_enable_24h);
 	}
 	if (wifiManager.nixie_params.count("setTimeManuallyFlag") == 1) {
 		uint8_t new_manual_time_flag = atoi(wifiManager.nixie_params["setTimeManuallyFlag"].c_str());
-		if (new_manual_time_flag != manual_time_flag) {
+		if (new_manual_time_flag != cfg_manual_time_flag) {
 			EEaddress = mem_map["manual_time_flag"];
 			uint8_t temp_time_flag = new_manual_time_flag;
 			EEPROM.put(EEaddress, temp_time_flag);
 		}
-		manual_time_flag = new_manual_time_flag;
+		cfg_manual_time_flag = new_manual_time_flag;
 		timeRefreshFlag = 1;
 	}
 	if (wifiManager.nixie_params.count("time") == 1) {
 		const char *new_time = wifiManager.nixie_params["time"].c_str();
-		if (new_time[0] != '\0' and new_time != _time) {
-			strcpy(_time, new_time);
+		if (new_time[0] != '\0' and new_time != cfg_time) {
+			strcpy(cfg_time, new_time);
 			timeRefreshFlag = 1;
 		}
 	}
 	if (wifiManager.nixie_params.count("date") == 1) {
 		const char *new_date = wifiManager.nixie_params["date"].c_str();
-		if (new_date[0] != '\0' and new_date != date) {
-			strcpy(date, new_date);
+		if (new_date[0] != '\0' and new_date != cfg_date) {
+			strcpy(cfg_date, new_date);
 			timeRefreshFlag = 1;
 		}
 	}
@@ -347,11 +347,11 @@ void updateParameters()
 void updateTime()
 {
 	if (timeRefreshFlag) {
-		if (manual_time_flag) { // I need feedback from the WiFiManager API that this option has been selected.
+		if (cfg_manual_time_flag) { // I need feedback from the WiFiManager API that this option has been selected.
 			NTP.stop(); // NTP sync is disableded to avoid sync errors.
 			int hours = -1;
 			int minutes = -1;
-			char *time_token = strtok(_time, ":");
+			char *time_token = strtok(cfg_time, ":");
 			while (time_token != NULL) {
 				if (hours == -1) {
 					hours = atoi(time_token);
@@ -363,7 +363,7 @@ void updateTime()
 			int year = -1;
 			int month = -1;
 			int day = -1;
-			char *date_token = strtok(date, "-");
+			char *date_token = strtok(cfg_date, "-");
 			while (date_token != NULL) {
 				if (year == -1) {
 					year = atoi(date_token);
